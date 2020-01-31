c6f89ada206082ba275b5624aac812a9e8e50f38
anishdass <anishdass@users.noreply.github.com>
2020-01-31 19:13:30 +0530

    Code file created.

---
+++ Data-wrangling-using-pandas/code.py
@@ -0,0 +1,93 @@
+# --------------
+# Import packages
+import numpy as np
+import pandas as pd
+from scipy.stats import mode 
+ 
+
+
+
+# code starts here
+bank=pd.read_csv(path)
+#df.head()
+
+categorical_var=bank.select_dtypes(include='object')
+print(categorical_var)
+
+numerical_var=bank.select_dtypes(include='number')
+print(numerical_var)
+# code ends here
+
+
+# --------------
+# code starts here
+banks=bank.drop(columns='Loan_ID')
+
+banks.head()
+
+print(banks.isnull().sum())
+
+bank_mode=banks.mode().iloc[0]
+
+banks.fillna(value=bank_mode, inplace=True)
+
+banks.head()
+
+print(banks.isnull().sum())
+#code ends here
+
+
+# --------------
+# Code starts here
+
+
+
+
+
+avg_loan_amount=banks.pivot_table(index=['Gender','Married','Self_Employed'],values='LoanAmount', aggfunc='mean')
+
+
+
+# code ends here
+
+
+
+# --------------
+# code starts here
+
+loan_approved_se=((banks['Self_Employed']=='Yes') & (banks['Loan_Status']=='Y')).sum()
+
+print(loan_approved_se)
+
+loan_approved_nse=((banks['Self_Employed']=='No') & (banks['Loan_Status']=='Y')).sum()
+
+print(loan_approved_nse)
+
+Loan_Status=614
+
+percentage_se=(loan_approved_se/Loan_Status)*100
+
+percentage_nse=(loan_approved_nse/Loan_Status)*100
+
+# code ends here
+
+
+# --------------
+# code starts here
+loan_term=banks['Loan_Amount_Term'].apply(lambda x: x/12)
+
+big_loan_term=(banks['Loan_Amount_Term'].apply(lambda x:x/12)>=25).sum()
+
+# code ends here
+
+
+# --------------
+# code starts here
+loan_groupby=banks.groupby('Loan_Status')
+
+loan_groupby=loan_groupby['ApplicantIncome','Credit_History']
+
+mean_values=loan_groupby.mean()
+# code ends here
+
+
